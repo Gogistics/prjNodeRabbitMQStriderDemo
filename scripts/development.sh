e71081549cb0b28933a773ffb397956fbb23ddd7
Alan Tai <gogistics@gogistics-tw.com>
2017-02-11 14:57:31 -0800

    Update files for Strider automation

---
+++ scripts/development.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Program:
+# spin up node app
+# History:
+# 02/05/2017
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export=PATH
+
+REDIS_PWD=''
+REDIS_HOST=''
+
+cd ./my_redis/
+while IFS='=' read -r key value
+do
+  keys+=("$key")
+  values+=("$value")
+  if [ ${key} == 'REDIS_PWD' ]; then
+    REDIS_PWD=${value}
+  fi
+  if [ ${key} == 'REDIS_HOSTNAME' ]; then
+    REDIS_HOST=${value}
+  fi
+done < ./my_vars
+
+cd ../
+NODE_ENV=development REDIS_PORT=6379 REDIS_HOST=${REDIS_HOST} REDIS_PASS=${REDIS_PWD} node .
---
+++ scripts/staging.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Program:
+# renew docker contaner
+# History:
+# 08/07/2016
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export=PATH
+REDIS_PWD=''
+REDIS_HOST=''
+
+cd ./my_redis/
+while IFS='=' read -r key value
+do
+  keys+=("$key")
+  values+=("$value")
+  if [ ${key} == 'REDIS_PWD' ]; then
+    REDIS_PWD=${value}
+  fi
+  if [ ${key} == 'REDIS_HOSTNAME' ]; then
+    REDIS_HOST=${value}
+  fi
+done < ./my_vars
+
+cd ../
+NODE_ENV=staging REDIS_PORT=6378 REDIS_HOST=${REDIS_HOST} REDIS_PASS=${REDIS_PWD} forever start .
